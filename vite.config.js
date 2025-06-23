1fa9bf4fe75ed6ca4a60f0b9a6e40ac07a928625
Neelu <clneelu2004@gmail.com>
2025-06-23 12:04:24 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 This is crucial!
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy" , // 👈 This is crucial!
 })
